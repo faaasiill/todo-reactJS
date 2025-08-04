1d6674f13e55dcf9ec6876f46decd92d1fe95d84
fasil <faazilrahmaan@gmail.com>
2025-08-04 19:15:29 +0530

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/todo-reactJS/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
